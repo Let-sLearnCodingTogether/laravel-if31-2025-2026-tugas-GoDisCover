68a6e1ffeab5528b33f62e31782d8104a4189d72
GoDisCover <jessenco122@gmail.com>
2025-10-21 16:07:36 +0700

    login butuh tutorial

--- React-Tugas-Jessen/src/auth/login.jsx
+++ React-Tugas-Jessen/src/auth/login.jsx
@@ -1,28 +1,52 @@
+import { useState } from "react";
 import { NavLink } from "react-router";
-
+  const [logins,setLogins] = useState({
+    email : "",
+    password : ""
+  })
+  const handlerOnChange = (event) => {
+    const {value, name} = event.target
+    setLogins({
+      ...logins,
+      [name]:value})
+  }
+  const onSubmit = async(event)=>{
+    event.preventDefault()
+        try {
+            setLoading(true)
+            const response = await http.post("/login",logins)
+            console.log(response.data);
+            
+        } catch (error) {
+            console.log(error);
+            
+        }finally{
+            setLoading(false)
+        }
+  }
 export default function login() {
   return (
     <div>
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 bg-blue-900">
-          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <div classname="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 bg-blue-900">
+          <div classname="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div classname="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 classname="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign in to your account
               </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
+              <form onSubmit={onSubmit} classname="space-y-4 md:space-y-6">
                 <div>
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                  <label for="email" classname="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                  <input value={form.email} onChange={handlerOnChange} name="email" classname="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                 </div>
                 <div>
-                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  <label for="password" classname="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                  <input value={form.password} onChange={handlerOnChange} name="password" classname="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                 </div>
-                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                <button type="submit" classname="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                <p classname="text-sm font-light text-gray-500 dark:text-gray-400">
                   Don’t have an account yet? 
                   <NavLink to={"/register"}>
-                   <a className="font-medium text-primary-600 hover:underline dark:text-primary-500"> Sign up</a>
+                   <a classnameName="font-medium text-primary-600 hover:underline dark:text-primary-500"> Sign up</a>
                   </NavLink>
                 </p>
               </form>
